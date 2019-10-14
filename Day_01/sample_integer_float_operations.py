990f9dd2cafc9c0660c6547bd4ddaa2750567e02
sunil sahu <sunil.sahu@netapp.com>
2019-10-14 20:41:36 +0530

    data_types content for day01

---
+++ Day_01/sample_integer_float_operations.py
@@ -0,0 +1,16 @@
+"""Test all the data types operations."""
+# Numeric operations
+#a = int(input("Enter value of 1st integer: "))
+a=20
+print("value of a: ", a)
+#b = int(input("Enter value of 1st integer: "))
+b=10
+print("value of b: ", b)        
+print("a + b: ", a + b)
+print("a - b: ", a - b)
+print("a * b: ", a * b)
+print("a / b: ", a / b)
+print("a // b: ", a // b)
+print("a % b: ", a % b)
+print("a ** b: ", a ** b) # pow(a,b)
+print("a==b: ", a==b)
---
+++ Day_01/sample_string_operations.py
@@ -0,0 +1,29 @@
+"""String operations"""
+# take input from user:
+#user_input = input('Enter data: ')
+s = "Starting with Python."
+print("value of s : ", s)
+print("x in s: ", "Python" in s)
+print("x not in s: ", "Python" not in s)
+print("s + t: ", s + " Good work")
+print("s * n: ", s * 2)
+print(15*"*")
+print("len(s): ", len(s))
+print("min(s), max(s): ", min(s), max(s))  # min would give ' '
+print("s.index(x): ", s.index('t')) # s.index('t', 4,10) in between 4 to 10
+print("s.count(x): ", s.count('t'))
+print("str(123): ", str(123))
+print("'*'.join(['ab', 'pq', 'rs']): ", '*'.join(['ab', 'pq', 'rs']))
+print("s.upper(): ", s.upper())
+print("s.lower(): ", s.lower())
+print("s.startswith('S'): ", s.startswith('S'))
+print("s.endswith('S'): ", s.endswith('S'))
+print("s.find('with'): ", s.find('with'))
+print("s.find('calsoft'): ", s.find('calsoft'))
+# slicing 
+print("s[3] : ", s[3])
+print ("s[2:4]: ", s[2:4])
+print("s[:] ", s[:])
+print("s[-1:3]: ", s[-1:3])
+print("s[::-1]: ", s[::-1])
+print("s[::-2]: ", s[::-2])
