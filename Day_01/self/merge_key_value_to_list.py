0947fdfe5105594919d3d267812da0f041482ffc
Pratik Raj <rajpratik71@gmail.com>
2019-10-24 20:41:12 +0530

    added some pratice programs for random concepts

---
+++ Day_01/self/merge_key_value_to_list.py
@@ -0,0 +1,13 @@
+test_list = [{'gfg': 2, 'is': 4, 'best': 6},
+             {'it': 5, 'is': 7, 'best': 8},
+             {'CS': 10}]
+
+print("The original list is : " + str(test_list))
+
+result = {}
+
+for sub in test_list:
+    for key, val in sub.items():
+        result.setdefault(key, []).append(val)
+
+print("The merged values encapsulated dictionary is : ", result)
